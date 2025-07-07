67b8853dd2b6834aca0cdc9676a250123ac0d348
MFab-1 <maciejfabianczyk@gmail.com>
2025-07-07 11:39:04 +0200

    adding a triangle on a screen

--- main.py
+++ main.py
@@ -3,19 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
 
 def main():
+    #init the screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    Spaceship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
-    x = 1
-    while x == 1:
+    #Main loop
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,0)
+        Player.draw(Spaceship, screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
     
